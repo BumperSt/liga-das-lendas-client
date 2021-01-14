1677abf8d74711df720206ca09af969ebf635037
Gabriel Mesquita <bumper@outlook.com.br>
2021-01-14 19:45:19 -0300

    Alterei url api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localhost:8080',
+    baseURL:'http://lvh.me:8080/',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
