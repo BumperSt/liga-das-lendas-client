efa99f590fc7c93373c5df99a9ea86301529b4cf
Gabriel Mesquita <bumper@outlook.com.br>
2022-01-11 13:44:48 -0300

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 //'https://liga-das-lendas-server-6h5gx42wjq-uc.a.run.app/'
+//'http://localhost:8080/'
 const api = axios.create({
-    baseURL:'http://localhost:8080/',
+    baseURL:'https://liga-das-lendas-server-6h5gx42wjq-uc.a.run.app/',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
