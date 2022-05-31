b49e53bb3b0036a2cc3bcae35b6dc733442e89a7
Gabriel <bumper@outlook.com.br>
2022-05-31 03:17:44 -0300

    updateLinkApi

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 //'https://liga-das-lendas-server-6h5gx42wjq-uc.a.run.app/'
-//'http://localhost:8080/'
+//'http://127.0.0.1:8080/'
 const api = axios.create({
-    baseURL:'http://127.0.0.1:8080/',
+    baseURL:'https://liga-das-lendas-server-6h5gx42wjq-uc.a.run.app/',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
