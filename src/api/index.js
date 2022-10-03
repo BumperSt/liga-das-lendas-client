9330e612d5e0ebdc2bdda4d00231fb405d6102a0
Gabriel <bumper@outlook.com.br>
2022-10-03 15:26:16 -0300

    updates

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-//'https://liga-das-lendas-server-6h5gx42wjq-uc.a.run.app/'
+//'https://liga-das-lendas-server-6h5gx42wjq-uc.a.run.app'
 //'http://127.0.0.1:8080/'
 const api = axios.create({
-    baseURL:'https://liga-das-lendas-server-6h5gx42wjq-uc.a.run.app/',
+    baseURL:'https://liga-das-lendas-server-6h5gx42wjq-uc.a.run.app',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
